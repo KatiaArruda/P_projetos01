f331da2b46b668a1b93df6e7c0a14f12e3b204ea
Katia Arruda | Desenvolvedora Front end <111231609+KatiaArruda@users.noreply.github.com>
2023-09-10 17:21:49 -0300

    Inserir a funcionalidade no Thema e copy

--- calc.js
+++ calc.js
@@ -75,17 +75,26 @@ document
   })
 
 document.getElementById('themeSwitcher').addEventListener('click', function () {
+  //Alterar o thema para claro
   if (main.dataset.theme === 'dark') {
-    root.style.setProperty('--bg-color', '#f1f5f9')
+    //cor: Roxo Claro
+    root.style.setProperty('--bg-color', '#e4e7f4')
+    //cor:cinza claro
     root.style.setProperty('--border-color', '#aaa')
-    root.style.setProperty('--font-color', '#212529')
-    root.style.setProperty('--primary-color', '#26834a')
+    //Cor:Cinza
+    root.style.setProperty('--font-color', '#808080')
+    //cor:vermelho claro
+    root.style.setProperty('--primary-color', '#ff6961')
     main.dataset.theme = 'light'
   } else {
-    root.style.setProperty('--bg-color', '#212529')
+    //Cor:Preto
+    root.style.setProperty('--bg-color', '#000000')
+    //Cor:marrom escuro
     root.style.setProperty('--border-color', '#666')
-    root.style.setProperty('--font-color', '#f1f5f9')
-    root.style.setProperty('--primary-color', '#4dff91')
+    //Cor:Roxo
+    root.style.setProperty('--font-color', '#EE82EE')
+    //Cor: Marrom rosado
+    root.style.setProperty('--primary-color', '#bc8f8f')
     main.dataset.theme = 'dark'
   }
 })
